10839721da0ef3ef30e2f1f7fe821ad1c4f7bb1f
Jason-ZW <zhenyang@rancher.com>
2018-07-30 17:04:03 +0800

    Add description to main funciton

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	contact := []string{"mailto:zhenyang@rancher.com"}
 
+	// Initialize ACME Client
 	a, err := acme.NewACMEClient("")
 	if err != nil {
 		logrus.Fatalf("new acme client error, reason: %v", err)
@@ -17,6 +18,7 @@ func main() {
 
 	logrus.Infof("acme initialize success. ACME Directory Info: %v", a.Dir)
 
+	// Create new ACME Account
 	accountCtx, accountCancel := context.WithTimeout(context.Background(), 1*time.Minute)
 	defer accountCancel()
 
@@ -27,6 +29,7 @@ func main() {
 
 	logrus.Infof("acme account initialize success. ACME Account Info: %v, KID: %s", account, a.Kid)
 
+	// Fetch ACME Account URL with given Key
 	accountURL, err := a.FetchAccountURL(accountCtx)
 	if err != nil {
 		logrus.Fatalf("fetch account error, reason: %v", err)
@@ -34,6 +37,7 @@ func main() {
 
 	logrus.Infof("acme account initialize success. ACME Account URL: %s, KID: %s", accountURL, a.Kid)
 
+	// Update ACME Account
 	contact = append(contact, "mailto:30165220@rancher.com")
 	accountUpdate, err := a.UpdateAccount(accountCtx, contact)
 	if err != nil {
